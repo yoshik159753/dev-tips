bd2a198a7d553e06d1940e79b980d86e43fda477
yoshik159753 <yoshik159753@users.noreply.github.com>
2021-08-29 05:40:36 +0000

    Deploying to gh-pages from @ yoshik159753/dev-tips@9f1bdd03a13229c5205a61f725f3dabe6cfa695e 🚀

--- adocToHtml.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-asciidoctor -D docs -o index.html ./src/main.adoc
-asciidoctor -D docs -o ubuntu.html ./src/ubuntu/main.adoc
-asciidoctor -D docs -o python.html ./src/python/main.adoc
-asciidoctor -D docs -o php.html ./src/php/main.adoc
