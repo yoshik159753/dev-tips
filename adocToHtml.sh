9c6c42c8cc942b24e806015c91d015091eba3859
Yoshi K <kai.outsider.777+git@gmail.com>
2022-02-09 20:13:02 +0900

    Add 5w2h

--- adocToHtml.sh
+++ adocToHtml.sh
@@ -5,3 +5,4 @@ asciidoctor -D docs -o python.html ./src/python/main.adoc
 asciidoctor -D docs -o php.html ./src/php/main.adoc
 asciidoctor -D docs -o asciidoc-cheatsheet.html ./src/asciidoc-cheatsheet/asciidoc-cheatsheet.adoc
 asciidoctor -D docs -o unittest-cheatsheet.html ./src/unittest-cheatsheet/main.adoc
+asciidoctor -D docs -o 5w2h.html ./src/5w2h/5w2h.adoc
